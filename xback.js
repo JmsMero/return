9f6d1e0ccb45497e5b70e8fe6ab6635da232743a
caobenfei <caobenfei@zhrt.com.cn>
2020-10-27 18:33:01 +0800

    提交

--- xback.js
+++ xback.js
@@ -197,8 +197,8 @@ var gameTool = {
   doBackInteractive: function() {
     var url = backBtnData.backInteractiveUrl;
     if (url != 'null') {
-
       localStorage.setItem("addrFrom",1);//这个设置是关键
+      alert('设置addrFrom：'+localStorage.getItem("addrFrom"))
       // window.removeEventListener('popstate', this.backChange, false);//false阻止默认事件
       // 添加标记是返回键打开的
       url.indexOf('?') > -1 ? url = url + '&openWayFrom=backBtn' : url = url + '?openWayFrom=backBtn';
