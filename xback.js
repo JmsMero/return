6d0cdaf013d90ccf8f262ef485af60399d3642fc
caobenfei <caobenfei@zhrt.com.cn>
2021-01-11 20:29:22 +0800

    提交702

--- xback.js
+++ xback.js
@@ -417,7 +417,11 @@ $(function () {
       success: function success(data) {
         isPrize = true;
         $('.specialWinPopup').remove();
-        $('body').append('<div class="specialWinPopup"><link rel="stylesheet" href="https://interactive-css.angpi.cn/1597975065098_tc08.css"><div class="tc-08 mask" style="display: block;"><div class="tc-close tc-close-btn" style="background-image:url(https://interactive-oss.angpi.cn/1591945187670_tc-08-close.png);"></div><div class="tc-container"><div class="tc-box"><div class="tc-prize tc-jump-btn" style="background-image:url(https://interactive-oss.angpi.cn/1596002677059_tc08-bg.gif);"></div></div></div></div><script>    var specialPopupScript = document.createElement("script");    specialPopupScript.type = "text/javascript";    specialPopupScript.onload = function() {setPersonalGamePopup()};    specialPopupScript.src = "https://interactive-js.angpi.cn/1610366607532_tc08.js";    document.querySelector(\'.specialWinPopup\').append(specialPopupScript);</script></div>');
+        try{
+          $('body').append('<div class="specialWinPopup"><link rel="stylesheet" href="https://interactive-css.angpi.cn/1597975065098_tc08.css"><div class="tc-08 mask" style="display: block;"><div class="tc-close tc-close-btn" style="background-image:url(https://interactive-oss.angpi.cn/1591945187670_tc-08-close.png);"></div><div class="tc-container"><div class="tc-box"><div class="tc-prize tc-jump-btn" style="background-image:url(https://interactive-oss.angpi.cn/1596002677059_tc08-bg.gif);"></div></div></div></div><script>    var specialPopupScript = document.createElement("script");    specialPopupScript.type = "text/javascript";    specialPopupScript.onload = function() {setPersonalGamePopup()};    specialPopupScript.src = "https://interactive-js.angpi.cn/1610366607532_tc08.js";    document.querySelector(\'.specialWinPopup\').append(specialPopupScript);</script></div>');
+        }catch(e){
+          console.log(e)
+        }
         $('.tc-close').click(function (){
           $('.tc-08').hide()
         })
